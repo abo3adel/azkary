b10e1bae61627e129f43100f9f5bdb4fc47ef827
abo3adel <abo3adel35@gmail.com>
2021-02-11 16:41:54 +0000

    fix sqlJs wasm content type header

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 import router from './router';
 
-import { IonicVue } from '@ionic/vue';
+import { IonicVue, getPlatforms } from '@ionic/vue';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/vue/css/core.css';
@@ -22,11 +22,23 @@ import '@ionic/vue/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
+import { createConnection } from 'typeorm';
 
 const app = createApp(App)
-  .use(IonicVue)
-  .use(router);
-  
+    .use(IonicVue)
+    .use(router);
+
 router.isReady().then(() => {
-  app.mount('#app');
+    console.log(...getPlatforms());
+    createConnection({
+        type: 'sqljs',
+        autoSave: false,
+        location: 'browser',
+        logging: ['error', 'query', 'schema'],
+        synchronize: true,
+        entities: [],
+    })
+        .then((r) => console.log(r))
+        .catch((e) => console.log(e));
+    app.mount('#app');
 });
---
+++ vue.config.js
@@ -0,0 +1,65 @@
+const webpack = require('webpack');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    devServer: {
+        // proxy: 'http://images.test',
+        before(app) {
+            // use proper mime-type for wasm files
+            app.get('*.wasm', function(req, res, next) {
+                var options = {
+                    root: contentBase,
+                    dotfiles: 'deny',
+                    headers: {
+                        'Content-Type': 'application/wasm',
+                    },
+                };
+                res.sendFile(req.url, options, function(err) {
+                    if (err) {
+                        next(err);
+                    }
+                });
+            });
+        },
+    },
+    chainWebpack: (webpackConfig) => {
+        // webpackConfig.module
+        //     .rule('wasm')
+        //     .test(/.wasm$/)
+        //     .use('wasm-loader')
+        //     .loader('wasm-loader');
+    },
+    configureWebpack: {
+        plugins: [
+            new webpack.NormalModuleReplacementPlugin(/typeorm$/, function(
+                result
+            ) {
+                result.request = result.request.replace(
+                    /typeorm/,
+                    'typeorm/browser'
+                );
+            }),
+            // new webpack.ProvidePlugin({
+            //     'window.SQL': 'sql.js/dist/sql-wasm.js',
+            // }),
+            new webpack.ProvidePlugin({
+                'window.SQL': 'sql.js/dist/sql-wasm-debug.js',
+            }),
+            new CopyPlugin([
+                {
+                    from: 'node_modules/sql.js/dist/sql-wasm-debug.wasm',
+                    to: '../sql-wasm-debug.wasm',
+                },
+            ]),
+            new webpack.ProvidePlugin({
+                'window.SQL': 'sql.js/dist/sql-wasm.js',
+            }),
+            new CopyPlugin([
+                {
+                    from: 'node_modules/sql.js/dist/sql-wasm.wasm',
+                    to: '../sql-wasm.wasm',
+                },
+            ]),
+        ],
+    },
+};
