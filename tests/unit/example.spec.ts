f0a44872631f8bc3c00c7018bef40624860ef061
abo3adel <abo3adel35@gmail.com>
2021-02-12 15:41:42 +0000

    removed unused test files

--- tests/unit/example.spec.ts
+++
@@ -1,9 +0,0 @@
-import { mount } from '@vue/test-utils'
-// import Tab1 from '@/views/Tab1.vue'
-
-describe('Tab1.vue', () => {
-  it('it is true', () => {
-    // const wrapper = mount(Tab1)
-    expect(true).toBe(true);
-  })
-})
