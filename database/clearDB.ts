79a2d3413a7f5c7ab718f7688c72cab539f5ceb3
abo3adel <abo3adel35@gmail.com>
2021-02-12 17:50:55 +0000

    reset db by deleting db file

--- database/clearDB.ts
+++ database/clearDB.ts
@@ -1,4 +1,3 @@
-import { User } from '@/entities/User';
 import db, { TEST_DB_NAME, ENTITIES } from '@/utils/db';
 import { BaseEntity, EntityTarget } from 'typeorm';
 
@@ -6,6 +5,11 @@ export default async function clearDB(
     entity: EntityTarget<BaseEntity> | null = null,
     conName: string = TEST_DB_NAME
 ) {
+    if (process.env.NODE_ENV === 'test') {
+        const nrc = require('node-run-cmd');
+        return await nrc.run('rm -rf browser');
+    }
+
     const con = await db(conName);
     if (null !== entity) {
         return await con
@@ -15,7 +19,13 @@ export default async function clearDB(
 
     for (let ent of ENTITIES) {
         const tableName = con.getMetadata(ent).tableName;
-        await con.query(`DELETE FROM ${tableName}`);
+        await con.query(
+            `DELETE FROM :tbName;
+            VACUUM;`,
+            [tableName]
+        );
+        await con.query(`
+        DELETE FROM SQLITE_SEQUENCE WHERE name = '${tableName}'`);
         // console.error("asdasd asd".repeat(90));
         // console.log(await (con.getRepository(ent).count()), conName);
     }
