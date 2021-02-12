6d31c2d0121262972a108e07d7a460ca068d17fd
abo3adel <abo3adel35@gmail.com>
2021-02-12 13:10:44 +0000

    deleted data from all tables before tests

--- tests/unit/clearDB.ts
+++ tests/unit/clearDB.ts
@@ -1,9 +1,26 @@
-import db from '@/utils/db';
+import { User } from '../../src/entities/User';
+import db, { TEST_DB_NAME } from '../../src/utils/db';
 import { BaseEntity, EntityTarget } from 'typeorm';
 
-export const TEST_DB_NAME = 'test';
+const entities: EntityTarget<BaseEntity>[] = [User];
 
-export default async function clearDB(entity: EntityTarget<BaseEntity>, conName: string = TEST_DB_NAME) {
+export default async function clearDB(
+    entity: EntityTarget<BaseEntity> | null = null,
+    conName: string = TEST_DB_NAME
+) {
     const con = await db(conName);
-    return await con.getRepository(entity).clear();
+    if (null !== entity) {
+        return await con
+            .getRepository(entity as EntityTarget<BaseEntity>)
+            .clear();
+    }
+
+    for (let ent of entities) {
+        const tableName = con.getMetadata(ent).tableName;
+        await con.query(`DELETE FROM ${tableName}`);
+        // console.error("asdasd asd".repeat(90));
+        // console.log(await (con.getRepository(ent).count()), conName);
+    }
+
+    return;
 }
