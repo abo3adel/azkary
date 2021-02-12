f21c29b628bb1a5a1497487396bc4e9ff8d5f531
abo3adel <abo3adel35@gmail.com>
2021-02-12 13:09:50 +0000

    set database connection name

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,7 +1,10 @@
 import { User } from '../entities/User';
 import { Connection, createConnection, getConnection } from 'typeorm';
 
-export default async function db(conName: string = 'default'): Promise<Connection> {
+export const TEST_DB_NAME = 'test';
+export const APP_DB_NAME = 'default';
+
+export default async function db(conName = APP_DB_NAME): Promise<Connection> {
     let con: Connection;
 
     try {
