244c7bd8f6eeac646c2f09c11a4243935571ea3a
abo3adel <abo3adel35@gmail.com>
2021-02-13 09:29:55 +0000

    remove default tabs

---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+    <h1>Home {{page}}</h1>
+</template>
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({})
+export default class Home extends Vue
+{
+    page = 'PAGE';
+}
+</script>
