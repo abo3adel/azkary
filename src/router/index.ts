48b90d0f058fce20dede0889a7b44a6013846032
abo3adel <abo3adel35@gmail.com>
2021-02-13 12:30:06 +0000

    show home page

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,30 +3,30 @@ import { RouteRecordRaw } from 'vue-router';
 import Tabs from '../views/Tabs.vue'
 
 const routes: Array<RouteRecordRaw> = [
+//   {
+//     path: '/',
+//     redirect: '/home'
+//   },
   {
     path: '/',
-    redirect: '/tabs/tab1'
-  },
-  {
-    path: '/tabs/',
     component: Tabs,
     children: [
       {
         path: '',
-        redirect: '/tabs/tab1'
+        component: () => import('@/views/Home.vue')
       },
-      {
-        path: 'tab1',
-        component: () => import('@/views/Tab1.vue')
-      },
-      {
-        path: 'tab2',
-        component: () => import('@/views/Tab2.vue')
-      },
-      {
-        path: 'tab3',
-        component: () => import('@/views/Tab3.vue')
-      }
+    //   {
+    //     path: 'tab1',
+    //     component: () => import('@/views/Tab1.vue')
+    //   },
+    //   {
+    //     path: 'tab2',
+    //     component: () => import('@/views/Tab2.vue')
+    //   },
+    //   {
+    //     path: 'tab3',
+    //     component: () => import('@/views/Tab3.vue')
+    //   }
     ]
   }
 ]
