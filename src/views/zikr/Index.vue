70d20cdab5634737eb8c36d54680106651d750db
abo3adel <abo3adel35@gmail.com>
2021-02-14 21:17:25 +0000

    show azkar categories

--- src/views/zikr/Index.vue
+++ src/views/zikr/Index.vue
@@ -1,21 +1,65 @@
 <template>
-<ion-page class='bg-page'>
+    <ion-page class="bg-page">
         <ion-header>
-            <ion-toolbar color='primary'>
-                <ion-back-button icon="home"></ion-back-button>
-                <h3 class='p-4 pb-8 uppercase bg-primary-600'>{{$t('zikr.toolbar')}}</h3>
+            <ion-toolbar color="primary">
+                <h3 class="p-4 pb-8 uppercase bg-primary-600">
+                    {{ $t('zikr.toolbar') }}
+                </h3>
+                <ion-buttons slot="end">
+                    <ion-back-button></ion-back-button>
+                </ion-buttons>
             </ion-toolbar>
         </ion-header>
-        <ion-content :fullscreen='true'>
-            <h1>asdsad</h1>
+        <ion-content :fullscreen="true">
+            <div class="pt-10">
+                <div
+                    v-for="(cat, inx) in categories"
+                    :key="cat.id"
+                    class="w-full"
+                >
+                    <ion-button
+                        :color="icons[inx].color"
+                        expand="block"
+                        class="mb-5"
+                    >
+                        <span class="absolute left-0">
+                            {{ cat.title }}
+                        </span>
+                        <ion-icon
+                            v-if="icons[inx].type === 'svg'"
+                            :src="`/assets/icons/${icons[inx].name}.svg`"
+                            class="absolute right-0 text-4xl"
+                        ></ion-icon>
+                        <ion-icon
+                            v-else
+                            :icon="icons[inx].name"
+                            class="absolute right-0 text-4xl"
+                        ></ion-icon>
+                    </ion-button>
+                </div>
+            </div>
         </ion-content>
-</ion-page> 
+    </ion-page>
 </template>
 <script lang="ts">
     import { Options, Vue } from 'vue-class-component';
     import db from '@/utils/db';
     import { Category } from '@/entities/Category';
-    import { IonToolbar, IonBackButton, IonPage, IonHeader, IonContent } from '@ionic/vue';
+    import {
+        IonToolbar,
+        IonBackButton,
+        IonPage,
+        IonHeader,
+        IonContent,
+        IonButtons,
+        IonList,
+        IonItem,
+        IonButton,
+        IonRippleEffect,
+        IonIcon,
+    } from '@ionic/vue';
+
+    import { sunnyOutline, cloudyNightOutline, alarmOutline } from 'ionicons/icons';
 
     @Options({
         components: {
@@ -23,15 +67,62 @@
             IonBackButton,
             IonPage,
             IonHeader,
-            IonContent
+            IonContent,
+            IonButtons,
+            IonList,
+            IonItem,
+            IonButton,
+            IonRippleEffect,
+            IonIcon,
         },
     })
     export default class ZikrIndex extends Vue {
+        categories: Category[] = [];
+        icons: { type: string; name?: string; color: string }[] = [
+            {
+                type: 'icon',
+                name: sunnyOutline,
+                color: 'gold',
+            },
+            {
+                type: 'icon',
+                name: cloudyNightOutline,
+                color: 'tertiary',
+            },
+            {
+                type: 'svg',
+                name: 'mosque',
+                color: 'secondary',
+            },
+            {
+                type: 'icon',
+                name: alarmOutline,
+                color: 'success',
+            },
+            {
+                type: 'svg',
+                name: 'half-moon',
+                color: 'danger',
+            },
+            {
+                type: 'svg',
+                name: 'julus',
+                color: 'primary',
+            },
+        ];
+
+        async loadCategories() {
+            const con = await db();
+            const categories = await con.createQueryBuilder()
+                .from(Category, 'Category')
+                .execute();
+
+            categories.pop();
+            this.categories = categories;
+        }
+
         mounted() {
-            db().then((con) => {
-                const repo = con.getRepository(Category);
-                repo.find().then((r) => console.log(r));
-            });
+            this.loadCategories();
         }
     }
 </script>
