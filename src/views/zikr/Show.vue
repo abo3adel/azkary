aaaf661591a8bd81c4ab2c039f7a98de5724e445
abo3adel <abo3adel35@gmail.com>
2021-02-15 00:51:45 +0000

    animate azkar list

--- src/views/zikr/Show.vue
+++ src/views/zikr/Show.vue
@@ -28,48 +28,52 @@
                 @ionItemReorder="doReorder($event)"
                 :disabled="true"
             >
-                <template v-for="(z, zinx) in category.azkar" :key="z.id">
-                    <ion-item-sliding v-if="z.count > 0">
-                        <ion-item-options side="start">
-                            <ion-item-option
-                                color="primary"
-                                @click="share(item)"
-                            >
-                                Share
-                            </ion-item-option>
-                        </ion-item-options>
+                <transition-group name="list" tag="div">
+                    <template v-for="(z, zinx) in category.azkar" :key="z.id">
+                        <transition name="slide-fade">
+                            <ion-item-sliding v-if="z.count > 0">
+                                <ion-item-options side="start">
+                                    <ion-item-option
+                                        color="primary"
+                                        @click="share(item)"
+                                    >
+                                        Share
+                                    </ion-item-option>
+                                </ion-item-options>
 
-                        <ion-item
-                            @click="z.count--"
-                            class="hover:cursor-pointer ion-activatable ripple-parent"
-                            :color="zinx % 2 == 0 ? 'light' : ''"
-                        >
-                            <ion-label
-                                style="white-space: break-spaces;word-wrap: break-all;"
-                            >
-                                {{ z.body }}
-                            </ion-label>
-                            <ion-ripple-effect></ion-ripple-effect>
-                            <ion-note
-                                slot="end"
-                                :color="meta.color"
-                                class="font-bold text-md"
-                            >
-                                {{ z.count }}
-                            </ion-note>
-                            <ion-reorder slot="end"></ion-reorder>
-                        </ion-item>
+                                <ion-item
+                                    @click="z.count--"
+                                    class="hover:cursor-pointer ion-activatable ripple-parent"
+                                    :color="zinx % 2 == 0 ? 'light' : ''"
+                                >
+                                    <ion-label
+                                        style="white-space: break-spaces;word-wrap: break-all;"
+                                    >
+                                        {{ z.body }}
+                                    </ion-label>
+                                    <ion-ripple-effect></ion-ripple-effect>
+                                    <ion-note
+                                        slot="end"
+                                        :color="meta.color"
+                                        class="font-bold text-md"
+                                    >
+                                        {{ z.count }}
+                                    </ion-note>
+                                    <ion-reorder slot="end"></ion-reorder>
+                                </ion-item>
 
-                        <ion-item-options side="end">
-                            <ion-item-option
-                                color="danger"
-                                @click="unread(item)"
-                            >
-                                Delete
-                            </ion-item-option>
-                        </ion-item-options>
-                    </ion-item-sliding>
-                </template>
+                                <ion-item-options side="end">
+                                    <ion-item-option
+                                        color="danger"
+                                        @click="unread(item)"
+                                    >
+                                        Delete
+                                    </ion-item-option>
+                                </ion-item-options>
+                            </ion-item-sliding>
+                        </transition>
+                    </template>
+                </transition-group>
             </ion-reorder-group>
         </ion-content>
     </ion-page>
@@ -94,6 +98,7 @@
         IonReorderGroup,
         IonReorder,
         IonIcon,
+        IonButtons,
     } from '@ionic/vue';
     import { Category } from '@/entities/Category';
     import db from '@/utils/db';
@@ -121,6 +126,7 @@
             IonReorderGroup,
             IonReorder,
             IonIcon,
+            IonButtons,
         },
     })
     export default class Show extends Vue.with(Props) {
@@ -167,4 +173,34 @@
         background: red;
         color:#fff;
     } */
+    .list-item {
+        display: inline-block;
+        margin-right: 10px;
+    }
+    .list-enter-active,
+    .list-leave-active {
+        transition: all 1s ease;
+    }
+    .list-enter-from,
+    .list-leave-to {
+        opacity: 0;
+        transform: translateX(-30px);
+    }
+
+    .list-move {
+        transition: transform 0.8s ease;
+    }
+    .slide-fade-enter-active {
+        transition: all 0.3s ease-out;
+    }
+
+    .slide-fade-leave-active {
+        transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+    }
+
+    .slide-fade-enter-from,
+    .slide-fade-leave-to {
+        transform: translateX(20px);
+        opacity: 0;
+    }
 </style>
