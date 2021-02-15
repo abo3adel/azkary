0c8fa68fef17856970468b2a6656fa3ccf37285c
abo3adel <abo3adel35@gmail.com>
2021-02-15 00:12:48 +0000

    slide zikr button

--- src/views/zikr/Show.vue
+++ src/views/zikr/Show.vue
@@ -3,7 +3,10 @@
         <ion-header>
             <ion-toolbar :color="meta.color || 'primary'">
                 <ion-buttons slot="start">
-                    <ion-back-button></ion-back-button>
+                    <ion-back-button
+                        default-href="/tabs/home"
+                        :text="$t('backBtn')"
+                    ></ion-back-button>
                 </ion-buttons>
                 <h3 class="p-4 pb-8 uppercase">
                     {{ $t(`zikr.cat.${category.slug}`) }}
@@ -21,13 +24,41 @@
             </ion-toolbar>
         </ion-header>
         <ion-content :fullscreen="true">
-            <ion-list>
-                <ion-item v-for="z in category.azkar" :key="z.id">
-                    <ion-label>
-                        {{ z.body }}
-                    </ion-label>
-                </ion-item>
-            </ion-list>
+            <ion-reorder-group
+                @ionItemReorder="doReorder($event)"
+                :disabled="false"
+            >
+                <ion-item-sliding v-for="z in category.azkar" :key="z.id">
+                    <ion-item-options side="start">
+                        <ion-item-option color="primary" @click="share(item)">
+                            Share
+                        </ion-item-option>
+                    </ion-item-options>
+
+                    <ion-item class="hover:cursor-pointer">
+                        <ion-label
+                            style="white-space: break-spaces;word-wrap: break-all;"
+                        >
+                            {{ z.body }}
+                        </ion-label>
+                        <ion-ripple-effect></ion-ripple-effect>
+                        <ion-note
+                            slot="end"
+                            :color="meta.color"
+                            class="text-lg font-bold"
+                        >
+                            {{ z.count }}
+                        </ion-note>
+                        <ion-reorder slot="end"></ion-reorder>
+                    </ion-item>
+
+                    <ion-item-options side="end">
+                        <ion-item-option color="danger" @click="unread(item)">
+                            Delete
+                        </ion-item-option>
+                    </ion-item-options>
+                </ion-item-sliding>
+            </ion-reorder-group>
         </ion-content>
     </ion-page>
 </template>
@@ -36,16 +67,25 @@
 
     import {
         IonPage,
+        IonBackButton,
         IonToolbar,
         IonHeader,
         IonContent,
         IonList,
         IonItem,
         IonLabel,
+        IonRippleEffect,
+        IonItemSliding,
+        IonItemOptions,
+        IonItemOption,
+        IonNote,
+        IonReorderGroup,
+        IonReorder,
     } from '@ionic/vue';
     import { Category } from '@/entities/Category';
     import db from '@/utils/db';
     import getCategoryIcon, { CategoryIcon } from '@/utils/getCategoryIcon';
+    import { Zikr } from '@/entities/Zikr';
 
     class Props {
         slug!: string;
@@ -54,23 +94,48 @@
     @Options({
         components: {
             IonPage,
+            IonBackButton,
             IonToolbar,
             IonHeader,
             IonContent,
             IonList,
             IonItem,
             IonLabel,
+            IonRippleEffect,
+            IonItemSliding,
+            IonItemOptions,
+            IonItemOption,
+            IonNote,
+            IonReorderGroup,
+            IonReorder,
         },
     })
     export default class Show extends Vue.with(Props) {
         category: Category = new Category();
         meta: CategoryIcon | null = null;
+        // items: Zikr[] = []
 
         async loadData() {
             const repo = await (await db()).getRepository(Category);
             this.category = await repo.findOneOrFail({
                 slug: this.$route.params.slug as string,
             });
+        }
+
+        doReorder(event: CustomEvent) {
+            // The `from` and `to` properties contain the index of the item
+            // when the drag started and ended, respectively
+            console.log(
+                'Dragged from index',
+                event.detail.from,
+                'to',
+                event.detail.to
+            );
+
+            // Finish the reorder and position the item in the DOM based on
+            // where the gesture ended. This method can also be called directly
+            // by the reorder group
+            event.detail.complete();
         }
 
         beforeMount() {
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,46 @@
-const colors = require('tailwindcss/colors')
+const colors = require('tailwindcss/colors');
 
 module.exports = {
-  purge: ['src/**/*.vue', 'public/**/*.html', 'src/**/*.ts'],
-  darkMode: 'class', // or 'media' or 'class'
-  theme: {
-    extend: {
-      colors: {
-        primary: {
-          '400': 'var(--ion-color-primary-tint)',
-          '600': 'var(--ion-color-primary)',
-          '800': 'var(--ion-color-primary-shade)',
+    purge: ['src/**/*.vue', 'public/**/*.html', 'src/**/*.ts'],
+    darkMode: 'class', // or 'media' or 'class'
+    theme: {
+        extend: {
+            colors: {
+                primary: {
+                    '400': 'var(--ion-color-primary-tint)',
+                    '600': 'var(--ion-color-primary)',
+                    '800': 'var(--ion-color-primary-shade)',
+                },
+                // primary: 'var(--ion-color-primary)',
+                gray: colors.blueGray,
+                teal: colors.teal,
+                orange: colors.orange,
+            },
+            backgroundColors: {
+                page: 'var(--page-bg)',
+                primary: 'var(--ion-color-primary)',
+            },
         },
-        // primary: 'var(--ion-color-primary)',
-        gray: colors.blueGray,
-        teal: colors.teal,
-        orange: colors.orange,
-      },
-      backgroundColors: {
-        page: 'var(--page-bg)',
-        primary: 'var(--ion-color-primary)'
-      }
-    }
-  },
-  variants: {
-    extend: {
-      
     },
-  },
-  plugins: [],
-}
+    variants: {
+        extend: {
+            animation: ['responsive', 'hover'],
+            backgroundColor: ['disabled', 'hover', 'focus', 'active'],
+            backgroundImage: ['disabled', 'responsive'],
+            borderColor: [
+                'disabled',
+                'hover',
+                'focus',
+                'active',
+                'group-hover',
+            ],
+            boxShadow: ['disabled', 'hover', 'focus', 'active'],
+            cursor: ['disabled', 'hover'],
+            gradientColorStops: ['responsive', 'hover', 'focus'],
+            opacity: ['responsive', 'hover', 'focus', 'disabled'],
+            outline: ['hover', 'focus', 'disabled'],
+            textColor: ['hover', 'disabled', 'group-hover'],
+        },
+    },
+    plugins: [],
+};
