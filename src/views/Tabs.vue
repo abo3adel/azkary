63d1eed396ebc72c8d00f8f1baf3f4390574c4f1
abo3adel <abo3adel35@gmail.com>
2021-02-26 02:03:20 +0000

    use solid icons on tabs bar

--- src/views/Tabs.vue
+++ src/views/Tabs.vue
@@ -1,45 +1,47 @@
 <template>
-  <ion-page>
-    <ion-tabs>
-      <ion-tab-bar slot="bottom">
-        <ion-tab-button tab="home" href='/tabs/home'>
-          <ion-icon :icon="home" />
-          <!-- <ion-label>Home</ion-label> -->
-        </ion-tab-button>
-          
-        <ion-tab-button tab="zikr" href="/tabs/zikr">
-          <ion-icon :icon="ellipse" />
-          <!-- <ion-label>Azkar</ion-label> -->
-        </ion-tab-button>
-        
-        <ion-tab-button tab="notify" href="/tabs/notify">
-          <ion-icon :icon="square" />
-          <!-- <ion-label>notify</ion-label> -->
-        </ion-tab-button>
+    <ion-page>
+        <ion-tabs>
+            <ion-tab-bar slot="bottom">
+                <ion-tab-button tab="home" href="/tabs/home">
+                    <ion-icon :icon="home" />
+                </ion-tab-button>
 
-        <ion-tab-button tab="sebha" href='/tabs/sebha'>
-          <ion-icon :icon="home" />
-          <!-- <ion-label>Home</ion-label> -->
-        </ion-tab-button>
-      </ion-tab-bar>
-    </ion-tabs>
-  </ion-page>
+                <ion-tab-button tab="zikr" href="/tabs/zikr">
+                    <ion-icon src="/assets/icons/night-mode.svg" />
+                </ion-tab-button>
+
+                <ion-tab-button tab="notify" href="/tabs/notify">
+                    <ion-icon src="/assets/icons/ramadan.svg" />
+                </ion-tab-button>
+
+                <ion-tab-button tab="sebha" href="/tabs/sebha">
+                    <ion-icon src="/assets/icons/prayer-beads.svg" />
+                </ion-tab-button>
+            </ion-tab-bar>
+        </ion-tabs>
+    </ion-page>
 </template>
 
 <script lang="ts">
-import { IonTabBar, IonTabButton, IonTabs,  IonIcon, IonPage } from '@ionic/vue';
-import { ellipse, square, triangle, home } from 'ionicons/icons';
+    import {
+        IonTabBar,
+        IonTabButton,
+        IonTabs,
+        IonIcon,
+        IonPage,
+    } from '@ionic/vue';
+    import { ellipse, square, triangle, home } from 'ionicons/icons';
 
-export default {
-  name: 'Tabs',
-  components: {  IonTabs, IonTabBar, IonTabButton, IonIcon, IonPage },
-  setup() {
-    return {
-      ellipse, 
-      square, 
-      triangle,
-      home
-    }
-  }
-}
+    export default {
+        name: 'Tabs',
+        components: { IonTabs, IonTabBar, IonTabButton, IonIcon, IonPage },
+        setup() {
+            return {
+                ellipse,
+                square,
+                triangle,
+                home,
+            };
+        },
+    };
 </script>
