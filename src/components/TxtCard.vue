1567bb41e710cc6f0cb277e3c9614bc46ac09d8c
abo3adel <abo3adel35@gmail.com>
2021-02-27 00:57:05 +0000

    add quran and hadith cards to home page

---
+++ src/components/TxtCard.vue
@@ -0,0 +1,138 @@
+<template>
+    <div
+        class="w-11/12 mx-auto bg-gray-200 rounded-md shadow-lg card dark:bg-gray-700"
+        :class="cls"
+    >
+        <div
+            class="flex rounded-md rounded-bl-none rounded-br-none bg-primary-600 text-color"
+        >
+            <h5 class="w-3/4 px-2 text-lg text-left header rtl:text-right">
+                {{ title }}
+            </h5>
+            <div class="w-1/4 text-left">
+                <ion-buttons
+                    class="absolute"
+                    :style="lang === 'ar' ? 'left: 0.5rem;' : 'right: 0.5rem;'"
+                >
+                    <ion-button>
+                        <ion-icon slot="start" :icon="shareSocialOutline" />
+                        <ion-label class="hidden sm:inline-block">
+                            {{ $t('home.btn.copy') }}
+                        </ion-label>
+                    </ion-button>
+                    <ion-button type="button" @click.prevent="showEn = !showEn">
+                        <ion-icon slot="start" :icon="language" />
+                        <ion-label class="hidden sm:inline-block">
+                            {{ $t('home.btn.trans') }}
+                        </ion-label>
+                    </ion-button>
+                </ion-buttons>
+            </div>
+        </div>
+        <div class="p-2">
+            <div class="text-xl text-right" :style="sty">
+                <p
+                    class="w-11/12 mx-auto font-semibold text-center text-red-600"
+                    v-if="error.ar"
+                >
+                    <ion-icon
+                        slot="start"
+                        :icon="alertCircleOutline"
+                        class="align-middle"
+                    />
+                    {{ $t('home.errNet') }}
+                </p>
+                <p>
+                    <span v-if="ar.length || !loading" dir="rtl">
+                        {{ ar }}
+                    </span>
+                    <span v-else>
+                        <ion-spinner
+                            class="w-11/12 mx-auto"
+                            name="crescent"
+                            color="primary"
+                        ></ion-spinner>
+                    </span>
+                </p>
+                <slot name="meta" />
+            </div>
+            <hr class="w-1/2 my-3 border border-gray-400" v-if="showEn" />
+            <div class="text-lg text-left" v-if="showEn">
+                <p
+                    class="w-11/12 mx-auto font-semibold text-center text-red-600"
+                    v-if="error.en"
+                >
+                    <ion-icon
+                        slot="start"
+                        :icon="alertCircleOutline"
+                        class="align-middle"
+                    />
+                    {{ $t('home.errNet') }}
+                </p>
+                <p>
+                    <span v-if="en.length || !loading" dir="ltr">
+                        {{ en }}
+                    </span>
+                    <span v-else>
+                        <ion-spinner
+                            class="w-11/12 mx-auto"
+                            name="crescent"
+                            color="primary"
+                        ></ion-spinner>
+                    </span>
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
+<script lang="ts">
+    import { Options, Vue, prop } from 'vue-class-component';
+    import {
+        IonButtons,
+        IonButton,
+        IonLabel,
+        IonSpinner,
+        IonIcon,
+    } from '@ionic/vue';
+    import {
+        shareSocialOutline,
+        language,
+        alertCircleOutline,
+    } from 'ionicons/icons';
+    import { Plugins } from '@capacitor/core';
+    const { Network } = Plugins;
+
+    class Props {
+        cls = prop<string>({ default: 'pt-3' });
+        title = prop<string>({ default: '' });
+        sty = prop<string>({ default: '' });
+        ar = prop<string>({ default: '' });
+        en = prop<string>({ default: '' });
+    }
+
+    @Options({
+        components: { IonButtons, IonButton, IonLabel, IonSpinner, IonIcon },
+        inject: ['lang'],
+    })
+    export default class TxtCard extends Vue.with(Props) {
+        loading = true;
+        showEn = false;
+        error = {
+            ar: false,
+            en: false,
+        };
+
+        shareSocialOutline = shareSocialOutline;
+        language = language;
+        alertCircleOutline = alertCircleOutline;
+
+        async mounted() {
+            const net = await Network.getStatus();
+            if (!net.connected) {
+                this.error = { ar: true, en: true };
+            }
+
+            // this.loading = false;
+        }
+    }
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -46,8 +46,33 @@
                 >
                     أذكارى
                 </h1>
-                <p class="pt-2 text-center text-gray-200" style="font-family: 'ArefRuqaa-Regular';">عطر فمك بذكر الله</p>
+                <p
+                    class="pt-2 text-center text-gray-200"
+                    style="font-family: 'ArefRuqaa-Regular';"
+                >
+                    عطر فمك بذكر اللّه
+                </p>
             </div>
+            <txt-card
+                :sty="`font-family: '${azkarFont}';line-height: 3.5rem;`"
+                cls="-mt-3"
+                :title="$t('home.quran')"
+                :ar="quran.ar"
+                :en="quran.en"
+            >
+                <template #meta>
+                    <p class="text-left">
+                        ({{ quran.surah }} - {{ quran.num }})
+                    </p>
+                </template>
+            </txt-card>
+
+            <txt-card
+                cls="mt-5"
+                :title="$t('home.hadith')"
+                :ar="hadith.ar"
+                :en="hadith.en"
+            />
         </ion-content>
     </ion-page>
 </template>
@@ -69,9 +94,11 @@
     import { Options, Vue } from 'vue-class-component';
     import { cogOutline, informationCircleOutline } from 'ionicons/icons';
     import getHijirDate from '@/common/getHijirDate';
+    import { Fonts } from '@/schema/UserEntity';
+    import TxtCard from '@/components/TxtCard.vue';
 
-    // import { Plugins } from '@capacitor/core';
-    // const { Storage } = Plugins;
+    import { Plugins } from '@capacitor/core';
+    const { Storage } = Plugins;
 
     @Options({
         components: {
@@ -85,12 +112,24 @@
             IonButtons,
             IonLabel,
             IonIcon,
+            TxtCard,
         },
         inject: ['lang'],
     })
     export default class Home extends Vue {
         hijir = '';
         bg = 'gold';
+        azkarFont: string = Fonts.Amiri;
+        quran = {
+            ar: '',
+            en: '',
+            surah: '',
+            num: '',
+        };
+        hadith = {
+            ar: '',
+            en: '',
+        };
 
         cogOutline = cogOutline;
         informationCircleOutline = informationCircleOutline;
@@ -98,11 +137,47 @@
         async mounted() {
             // @ts-ignore
             this.hijir = getHijirDate(this.lang);
-            console.log(new Date().getHours());
 
             if (new Date().getHours() > 12) {
                 this.bg = 'tertiary';
             }
+
+            this.azkarFont =
+                (await Storage.get({ key: 'azkarFont' })).value ??
+                this.azkarFont;
+
+            // load quran
+            // let res = await (
+            //     await fetch('http://api.alquran.cloud/v1/ayah/2:255/ar.asad')
+            // ).json();
+            // this.quran.ar = res.data.text;
+            // this.quran.surah = res.data.surah.name;
+            // this.quran.num = res.data.numberInSurah;
+
+            // res = await (
+            //     await fetch('http://api.alquran.cloud/v1/ayah/2:255/en.asad')
+            // ).json();
+            // this.quran.en = res.data.text;
+
+            this.quran.ar = "اللَّهُ لَا إِلَٰهَ إِلَّا هُوَ الْحَيُّ الْقَيُّومُ ۚ لَا تَأْخُذُهُ سِنَةٌ وَلَا نَوْمٌ ۚ لَهُ مَا فِي السَّمَاوَاتِ وَمَا فِي الْأَرْضِ ۗ مَنْ ذَا الَّذِي يَشْفَعُ عِنْدَهُ إِلَّا بِإِذْنِهِ ۚ يَعْلَمُ مَا بَيْنَ أَيْدِيهِمْ وَمَا خَلْفَهُمْ ۖ وَلَا يُحِيطُونَ بِشَيْءٍ مِنْ عِلْمِهِ إِلَّا بِمَا شَاءَ ۚ وَسِعَ كُرْسِيُّهُ السَّمَاوَاتِ وَالْأَرْضَ ۖ وَلَا يَئُودُهُ حِفْظُهُمَا ۚ وَهُوَ الْعَلِيُّ الْعَظِيمُ";
+            this.quran.surah = 'سُورَةُ البَقَرَةِ';
+            this.quran.num = '255';
+            this.quran.en = 'GOD - there is no deity save Him, the Ever-Living, the Self-Subsistent Fount of All Being. Neither slumber overtakes Him, nor sleep. His is all that is in the heavens and all that is on earth. Who is there that could intercede with Him, unless it be by His leave? He knows all that lies open before men and all that is hidden from them, whereas they cannot attain to aught of His knowledge save that which He wills [them to attain]. His eternal power overspreads the heavens and the earth, and their upholding wearies Him not. And he alone is truly exalted, tremendous.';
+
+
+            // hadith
+            // const hadith = await (
+            //     await fetch('https://api.sunnah.com/v1/hadiths/random', {
+            //         headers: {
+            //             'X-API-Key': 'SqD712P3E82xnwOAEOkGd5JZH8s9wRR24TqNFzjk',
+            //         },
+            //     })
+            // ).json();
+
+            this.hadith.ar =
+                ' وعنه أن رسول الله صلى الله عليه وسلم قال وهو على المنبر، وذكر الصدقة والتعفف عن المسألة‏:‏ ‏"‏اليد العليا خير من اليد السفلى‏.‏ واليد العليا هي المنفقة، والسفلى هي السائلة‏"‏ ‏(‏‏(‏متفق عليه‏)‏‏)‏‏.‏';
+            this.hadith.en =
+                'Ibn \'Umar (May Allah be pleased with them) reported: While Messenger of Allah (ﷺ) was on his pulpit (in the mosque) delivering a Khutbah (religious talk) about Sadaqah (charity) and begging, he said, "The upper hand is better than the lower hand, the upper hand is the one which gives and the lower hand is the begging one.';
         }
     }
 </script>
