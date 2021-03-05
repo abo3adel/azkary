41b6e7cb0f13f0ac7e2d1f2dfc07e8ab38ccc6a0
abo3adel <abo3adel35@gmail.com>
2021-03-05 15:29:04 +0000

    use native spinner in hybird devices

--- src/utils/loader.ts
+++ src/utils/loader.ts
@@ -1,22 +1,30 @@
 import { Plugins } from '@capacitor/core';
-import { loadingController } from '@ionic/vue';
+import { loadingController, isPlatform } from '@ionic/vue';
+import { SpinnerDialog } from '@ionic-native/spinner-dialog';
 
-const {Storage} = Plugins;
+const { Storage } = Plugins;
 
 export const LOADER_MESSAGE_AR = 'يرجى الإنتظار ...';
 export const LOADER_MESSAGE_EN = 'please wait ...';
 
 export class Loader {
     private loader!: HTMLIonLoadingElement | null;
-    private timeout = 8000; // 8 sec
+    private timeout = 5000; // 5 sec
     private locale = 'ar';
 
     public constructor() {
-        Storage.get({ key: 'lang' }).then((r) => (this.locale = r.value ?? this.locale));
+        Storage.get({ key: 'lang' }).then(
+            (r) => (this.locale = r.value ?? this.locale)
+        );
     }
 
     public async show(locale = this.locale): Promise<void> {
         const message = locale === 'ar' ? LOADER_MESSAGE_AR : LOADER_MESSAGE_EN;
+        if (isPlatform('hybrid')) {
+            SpinnerDialog.show(undefined, message, true);
+            return;
+        }
+
         if (!this.loader) {
             this.loader = await loadingController.create({
                 duration: this.timeout,
@@ -37,6 +45,11 @@ export class Loader {
     }
 
     public async hide() {
+        if (isPlatform('hybrid')) {
+            SpinnerDialog.hide();
+            return;
+        }
+
         if (this.loader) {
             await this.loader.dismiss();
             this.loader = null;
