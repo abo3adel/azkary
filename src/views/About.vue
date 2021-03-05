c7c68feee0dfb17f4c3dd79af454f33e066fcead
abo3adel <abo3adel35@gmail.com>
2021-03-05 22:11:18 +0000

    add footer with copyrights

--- src/views/About.vue
+++ src/views/About.vue
@@ -27,15 +27,60 @@
             <h1 class="mt-3 text-3xl">
                 {{ $t('app.slideName') }}
             </h1>
-            <hr class="w-1/4 h-1 mx-auto my-6 bg-gray-500" />
+            <hr class="w-1/4 mx-auto my-6 border border-gray-700" />
             <h3 class="text-2xl">{{ $t('app.download') }}</h3>
             <p class="mt-4 text-lg">
                 {{ $t('app.slide.pcMess') }}
             </p>
-            <ion-button color="primary" @click.prevent="openLink" class="mt-3">
+            <ion-button
+                color="primary"
+                @click.prevent="openLink('http://abo3adel.github.io')"
+                class="mt-3"
+            >
                 {{ $t('app.slide.pcCopy') }}
             </ion-button>
         </ion-content>
+        <div class="p-2 mt-3 text-white bg-gray-900 footer" dir="ltr">
+            <p>
+                App repository @
+                <a href="https://github.com/abo3adel/myazkar" target="_blank"
+                    >GITHUB</a
+                >.
+            </p>
+            <hr class="w-1/2 my-1 border border-gray-500" />
+            <div>
+                <p>
+                    Content &copy; from
+                    <a href="https://www.islambook.com/" target="_blank"
+                        >Islam Book</a
+                    >.
+                </p>
+                <hr class="w-1/2 my-1 border border-gray-500" />
+                <p>
+                    Icons made by
+                    <a
+                        href="https://www.freepik.com"
+                        title="Freepik"
+                        target="_blank"
+                        >Freepik</a
+                    >
+                    from
+                    <a
+                        href="https://www.flaticon.com/"
+                        title="Flaticon"
+                        target="_blank"
+                        >flaticon.com</a
+                    >.
+                </p>
+                <hr class="w-1/2 my-1 border border-gray-500" />
+                <p>
+                    Made with <span class="text-red-500">&hearts;</span> by
+                    <a href="http://abo3adel.github.io/" target="_blank"
+                        >TheNinjaCoder</a
+                    >
+                </p>
+            </div>
+        </div>
     </ion-page>
 </template>
 <script lang="ts">
@@ -66,8 +111,13 @@
         home = home;
         informationCircleOutline = informationCircleOutline;
 
-        openLink() {
-            window.open('http://abo3adel.github.io', '_blank');
+        openLink(url: string) {
+            window.open(url, '_blank');
         }
     }
 </script>
+<style lang="postcss">
+    p a {
+        @apply text-primary-600;
+    }
+</style>
