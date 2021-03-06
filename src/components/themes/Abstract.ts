70ebea7e8fb4a0f6bb99339287d7fbca903b4bd5
abo3adel <abo3adel35@gmail.com>
2021-03-06 02:10:30 +0000

    highlight more versions of allahum word

--- src/components/themes/Abstract.ts
+++ src/components/themes/Abstract.ts
@@ -13,21 +13,19 @@ export function highliteText(
     theme: string,
     color: string
 ): string {
+    const tag = (txt: any) =>
+        `<span class='text-primary-600' style="${
+            theme.indexOf('colored') > -1
+                ? `color: var(--ion-color-${color})`
+                : ''
+        }">${txt}</span>`;
+
     return txt
-        .replace(
-            'اللَّهُمَّ',
-            `<span class='text-primary-600' style="${
-                theme.indexOf('colored') > -1
-                    ? `color: var(--ion-color-${color})`
-                    : ''
-            }">اللَّهُمَّ</span>`
-        )
-        .replace(
-            'رَبَّنَا',
-            `<span class='text-primary-600' style="${
-                theme.indexOf('colored') > -1
-                    ? `color: var(--ion-color-${color})`
-                    : ''
-            }">رَبَّنَا</span>`
-        );
+        .trim()
+        .replace(/^اللَّهُمَّ/u, tag('اللَّهُمَّ'))
+        .replace(/^اللّهُـمَّ/u, tag('اللّهُـمَّ'))
+        .replace(/^اللّهـمَّ/u, tag('اللّهـمَّ'))
+        .replace(/^اللَّهُمَّ/u, tag('اللَّهُمَّ'))
+        .replace(/^(?:")رَبَّنَا/u, tag('رَبَّنَا'))
+        .replace(/^(?:")ربَّنَا/u, tag('ربَّنَا'));
 }
