e2c95262cad3a53c2e8ec238b5d55923682c3a27
abo3adel <abo3adel35@gmail.com>
2021-03-06 21:30:57 +0000

    load quran aya from quran.com api

--- src/components/home/Quran.vue
+++ src/components/home/Quran.vue
@@ -19,7 +19,7 @@
     const { Storage, Network } = Plugins;
 
     class Props {
-        azkarFont = prop<string>({required: true})
+        azkarFont = prop<string>({ required: true });
     }
 
     @Options({ components: { TxtCard } })
@@ -30,6 +30,68 @@
             surah: '',
             num: '',
         };
+
+        random(min: number, max: number): number {
+            return Math.floor(Math.random() * (max - min + 1) + min);
+        }
+
+        get ayaCount(): number[] {
+            return [
+                7,
+                286,
+                200,
+                176,
+                120,
+                165,
+                206,
+                75,
+                129,
+                109,
+                123,
+                111,
+                43,
+                52,
+                99,
+                128,
+                111,
+                110,
+                98,
+                135,
+                112,
+                78,
+                118,
+                64,
+                77,
+                227,
+                93,
+                88,
+                69,
+                60,
+                34,
+                30,
+                73,
+                54,
+                45,
+                83,
+                182,
+                88,
+                75,
+                85,
+                54,
+                53,
+                89,
+                59,
+                37,
+                35,
+                38,
+                29,
+                18,
+                45,
+                60,
+                49,
+                62,
+            ];
+        }
 
         async mounted() {
             const net = (await Network.getStatus()).connected;
@@ -61,26 +123,33 @@
                 return;
             }
 
+            const surah = this.random(2, 50);
+            const aya = this.random(1, this.ayaCount[surah]);
+
             // load quran
-            // let res = await (
-            //     await fetch('http://api.alquran.cloud/v1/ayah/2:255/ar.asad')
-            // ).json();
-            // this.quran.ar = res.data.text;
-            // this.quran.surah = res.data.surah.name;
-            // this.quran.num = res.data.numberInSurah;
+            let res = await (
+                await fetch(
+                    `http://api.alquran.cloud/v1/ayah/${surah}:${aya}/ar.asad`
+                )
+            ).json();
+            this.quran.ar = res.data.text;
+            this.quran.surah = res.data.surah.name;
+            this.quran.num = res.data.numberInSurah;
 
-            // res = await (
-            //     await fetch('http://api.alquran.cloud/v1/ayah/2:255/en.asad')
-            // ).json();
-            // this.quran.en = res.data.text;
+            res = await (
+                await fetch(
+                    `http://api.alquran.cloud/v1/ayah/${surah}:${aya}/en.asad`
+                )
+            ).json();
+            this.quran.en = res.data.text;
 
-            this.quran.ar =
-                'اللَّهُ لَا إِلَٰهَ إِلَّا هُوَ الْحَيُّ الْقَيُّومُ ۚ لَا تَأْخُذُهُ سِنَةٌ وَلَا نَوْمٌ ۚ لَهُ مَا فِي السَّمَاوَاتِ وَمَا فِي الْأَرْضِ ۗ مَنْ ذَا الَّذِي يَشْفَعُ عِنْدَهُ إِلَّا بِإِذْنِهِ ۚ يَعْلَمُ مَا بَيْنَ أَيْدِيهِمْ وَمَا خَلْفَهُمْ ۖ وَلَا يُحِيطُونَ بِشَيْءٍ مِنْ عِلْمِهِ إِلَّا بِمَا شَاءَ ۚ وَسِعَ كُرْسِيُّهُ السَّمَاوَاتِ وَالْأَرْضَ ۖ وَلَا يَئُودُهُ حِفْظُهُمَا ۚ وَهُوَ الْعَلِيُّ الْعَظِيمُ';
+            // this.quran.ar =
+            //     'اللَّهُ لَا إِلَٰهَ إِلَّا هُوَ الْحَيُّ الْقَيُّومُ ۚ لَا تَأْخُذُهُ سِنَةٌ وَلَا نَوْمٌ ۚ لَهُ مَا فِي السَّمَاوَاتِ وَمَا فِي الْأَرْضِ ۗ مَنْ ذَا الَّذِي يَشْفَعُ عِنْدَهُ إِلَّا بِإِذْنِهِ ۚ يَعْلَمُ مَا بَيْنَ أَيْدِيهِمْ وَمَا خَلْفَهُمْ ۖ وَلَا يُحِيطُونَ بِشَيْءٍ مِنْ عِلْمِهِ إِلَّا بِمَا شَاءَ ۚ وَسِعَ كُرْسِيُّهُ السَّمَاوَاتِ وَالْأَرْضَ ۖ وَلَا يَئُودُهُ حِفْظُهُمَا ۚ وَهُوَ الْعَلِيُّ الْعَظِيمُ';
 
-            this.quran.surah = 'سُورَةُ البَقَرَةِ';
-            this.quran.num = '255';
-            this.quran.en =
-                'GOD - there is no deity save Him, the Ever-Living, the Self-Subsistent Fount of All Being. Neither slumber overtakes Him, nor sleep. His is all that is in the heavens and all that is on earth. Who is there that could intercede with Him, unless it be by His leave? He knows all that lies open before men and all that is hidden from them, whereas they cannot attain to aught of His knowledge save that which He wills [them to attain]. His eternal power overspreads the heavens and the earth, and their upholding wearies Him not. And he alone is truly exalted, tremendous.';
+            // this.quran.surah = 'سُورَةُ البَقَرَةِ';
+            // this.quran.num = '255';
+            // this.quran.en =
+            //     'GOD - there is no deity save Him, the Ever-Living, the Self-Subsistent Fount of All Being. Neither slumber overtakes Him, nor sleep. His is all that is in the heavens and all that is on earth. Who is there that could intercede with Him, unless it be by His leave? He knows all that lies open before men and all that is hidden from them, whereas they cannot attain to aught of His knowledge save that which He wills [them to attain]. His eternal power overspreads the heavens and the earth, and their upholding wearies Him not. And he alone is truly exalted, tremendous.';
 
             Storage.set({
                 key: 'quran_aya',
